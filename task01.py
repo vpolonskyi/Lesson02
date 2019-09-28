854fd33897c3dc1bab3eb5bcae4f16e7b922a57b
Viacheslav Polonskyi <slon@4m.net.ua>
2019-09-28 10:57:24 +0300

    @elpiankova

--- task01.py
+++ task01.py
@@ -7,13 +7,13 @@ b = a.strip()
 b = b.replace(".", "")
 c = b.replace("-", "")
 
-if c.isdigit() and b.find("-") == b.rfind("-") and b.find("-") <= 0 and a.find(".") == a.rfind("."):
+if c.isdigit() and b.count("-") <= 1 and b.find("-") <= 0 and a.count(".") <= 1:
     print("Ба, да это ж число:", a.strip())
 else:
     print(a, "- это не число")
 
-print("В строке пробелов:", len(a) - len(a.replace(" ", "")))
-print("В строке точек:", len(a) - len(a.replace(".", "")))
+print("В строке пробелов:", a.count(" "))
+print("В строке точек:", a.count("."))
 
 if len(a) < 99:
     print("Строка длинною", len(a.center(100)), "символов +", a.center(100), "+")
@@ -23,3 +23,4 @@ else:
     print("Слишком длинная строка что б 'обложить' пробелами :(")
 
 print("Все с большой буквы:", a.title())
+
